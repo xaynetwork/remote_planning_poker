a00440bb7775e8952254f527e76a969b8d8b7d45
Pawel Biernacki <pawel@robotygraficzne.com>
2022-04-19 07:06:29 +0200

    added tailwind setup

---
+++ dev.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# for this to work we need to build the tailwind file at least once
+
+pushd frontend
+tailwindcss -c tailwind.config.js -o tailwind.css
+popd
+
+
+(trap 'kill 0' SIGINT; \
+  bash -c 'cd frontend; tailwindcss -c tailwind.config.js -o tailwind.css --watch' & \
+  bash -c 'cd frontend; trunk serve' & \
+  bash -c 'cd api; cargo watch -x run')
